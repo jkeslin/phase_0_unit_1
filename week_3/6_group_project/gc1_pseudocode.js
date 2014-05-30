6bdb975986349e41c57dc6f0e2d7645e572adfb9
Jeff Keslin <jkeslin@gmail.com>
2014-05-30 08:37:01 -0500

    pseudocode

---
+++ week_3/6_group_project/gc1_pseudocode.js
@@ -0,0 +1,34 @@
+// My role in the group is:
+//Person 2: Pseudocode
+
+// Here is my part of the challenge:
+
+//Function name: sum
+
+//Initialize function named "sum" that accepts an array as a parameter
+//initialze variable "add" for adding terms
+//for "i" is less than the length of the array
+//add the value at the index of the array "i" to the variable "add"
+//take result of loop and print the total
+
+
+//Function name: mean
+
+//Initialize function named "mean" that accepts an array as a parameter
+//initialize variable "sum"
+//for "i" is less than the length of the array
+//add the value at index "i" to the variable "sum"
+//take result of loop and divide by the length of the array
+//print the result
+
+
+//Function name: median
+
+//Initialize function named "median" that accepts an array as a parameter
+//Define variable "half" that finds half of the length of the array
+//sort array in order from least to greatest
+//if the modulus of half of the length of the array is not equal to zero
+//then define the "median" as the value at the index of the array that is located at the value of "half" minus 0.5
+//else
+//calculate the "median" as the sum of the value of the index at "half" minus 1, and "half", and divide by 2.
+//print results of "median"
