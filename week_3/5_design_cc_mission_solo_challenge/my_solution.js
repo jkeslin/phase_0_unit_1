c062e3c7af89e0860eb13edfd932802ece427008
Jeff Keslin <jkeslin@gmail.com>
2014-05-30 21:42:54 -0500

    finish pseudo

--- week_3/5_design_cc_mission_solo_challenge/my_solution.js
+++ week_3/5_design_cc_mission_solo_challenge/my_solution.js
@@ -3,7 +3,7 @@
 // Your mission description:
 // Ned Stark must travel from Winterfell to King's Landing
 // and slay Prince Joffrey. Then Return to Winterfell and 
-// proclaim that King's Landing is now ruled by House Stark and that he is the true Ruler of The 7 Kingdoms!
+// proclaim that King's Landing is now ruled by House Stark and that he is the one true Ruler of The 7 Kingdoms!
 
 // Pseudocode
 // define object constructor function to create characters
@@ -14,6 +14,10 @@
 // Ruler, Location
 // the Location variable should be an object with parameters x, y
 // 
+// use Move method to change Ned Stark's location from Winterfell to King's Landing
+// use Duel Mehod to slay prince Joffrey and change Ruler of King's Landing to Ned Stark, and delete title of Joffrey.
+// use Move method to return Ned Stark's location to Winterfell
+// print Ned Stark's quote to the console
 
 // Initial Code
 
